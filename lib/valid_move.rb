4e783bde437576c5fe4e1df64db61b99c6ee6b5e
Patrick Colina <trick.co@yahoo.com>
2019-12-15 22:54:49 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,7 @@
 require_relative '../lib/position_taken.rb'
 
 def valid_move?(board, index)
-  if index.between?(-1, 9) != true
-    false
-  elsif position_taken?(board, index) == true
-    false
-  elsif position_taken?(board, index) == false
+  if index.between?(-1, 9) && !position_taken?(board, index)
     true
   end
 end
